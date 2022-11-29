36612f946a8f8810a66f896679118b26fd0966df
Md. Latiful Kabir <latifulkabir567@gmail.com>
2022-11-30 03:18:58 +0600

    decrease quantity fixed2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Home from './pages/Home/Home';
 import UseReducer from './pages/Home/UseReducer';
-import PatientManagement from './pages/PatientManagement/PatientManagement';
 
 function App() {
   return (
     <div className="App">
       <UseReducer />
       <Home />
-      {/* <PatientManagement /> */}
     </div>
   );
 }
